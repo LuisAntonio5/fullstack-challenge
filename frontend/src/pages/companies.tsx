0c77fa20a705a5cdcd82dedf9db48484c1dfcd95
Luís António <luis.antonio.lima.silva@gmail.com>
2024-02-05 17:55:43 +0000

    Add navbar and make companies searchable by name and country

--- frontend/src/pages/companies.tsx
+++ frontend/src/pages/companies.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
 import CompanyCard from '../components/companyCard';
 
 const Companies = () => {
   const [companies, setCompanies] = useState([]);
+  const [searchQueryName, setSearchQueryName] = useState('');
+  const [searchQueryCountry, setSearchQueryCountry] = useState('');
 
   const fetchCompanies = async () => {
     try {
@@ -18,12 +21,43 @@ const Companies = () => {
     fetchCompanies();
   }, []);
 
+  const filteredCompanies = companies.filter((company) => {
+    const companyName = company.name ? company.name.toLowerCase() : "";
+    const companyCountry = company.country ? company.country.toLowerCase() : "";
+
+    return (
+      companyName.includes(searchQueryName.toLowerCase()) &&
+      companyCountry.includes(searchQueryCountry.toLowerCase())
+    );
+  });
+
   return (
     <div>
-      <h1>Companies</h1>
-      {companies.map((company) => (
-        <CompanyCard key={company.company_id} company={company} />
-      ))}
+      <div className="navbar">
+        <Link className='homeBtn' href={`/`}><h1>Home</h1></Link>
+        <Link className='homeBtn' href={`/deals`}><h1>Deals</h1></Link>
+      </div>
+      <h1 className='text-center'>Companies</h1>
+      <p className='text-center'>try clicking a company card!</p>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search by name"
+            value={searchQueryName}
+            onChange={(e) => setSearchQueryName(e.target.value)}
+          />
+        </div>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search by country"
+            value={searchQueryCountry}
+            onChange={(e) => setSearchQueryCountry(e.target.value)}
+          />
+        </div>
+        {filteredCompanies.map((company) => (
+          <CompanyCard key={company.company_id} company={company} />
+        ))}
     </div>
   );
 };
