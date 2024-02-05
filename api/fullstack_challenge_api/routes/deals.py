95b2388312bce1b78fee5a6464a5f8315ca296a5
Luís António <luis.antonio.lima.silva@gmail.com>
2024-02-05 17:20:38 +0000

    Add get_deals_by_company_id endpoint

--- api/fullstack_challenge_api/routes/deals.py
+++ api/fullstack_challenge_api/routes/deals.py
@@ -10,3 +10,14 @@ async def get_deals(db: Connection = Depends(get_db)):
     cursor.execute("SELECT * FROM deals")
     deals = cursor.fetchall()
     return {"deals": deals}
+
+@router.get("/deals/company/{company_id}")
+async def get_deals_by_company_id(company_id: int, db: Connection = Depends(get_db)):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM deals WHERE company_id = ?", (company_id,))
+    deals = cursor.fetchall()
+    
+    if deals:
+        return {"deals": deals}
+    else:
+        return {"message": "Deals for the company not found"}
